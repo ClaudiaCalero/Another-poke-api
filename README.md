18bf87904da008c298d9b4928fabc26c62406cad
ClaudiaCalero <claudiaclrduro@gmail.com>
2022-02-21 09:29:17 +0100

    first commit

--- README.md
+++
@@ -1,1 +0,0 @@
-# Another-poke-api
---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import PokeCardList from "./components/PokeCardList";
+
+const App = () => {
+  return (
+    <div className="app-container">
+      <h1>PokeApi</h1>
+      <PokeCardList />
+    </div>
+  );
+};
+
+export default App;
